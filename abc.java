91b3358af465c07ad9a68666bbad8f67686becb7
sidharth <sidhu005india@gmail.com>
2026-02-14 14:09:18 +0530

    commit by sid

--- abc.java
+++ abc.java
@@ -6,6 +6,7 @@ public class abc {
     public static void main(String[] args) {
         System.out.println("inside intellij");
         System.out.println("time kharab hai" );
+        System.out.println("time sahi hoga " );
     }
 
 
