2f4b6ec67d5d185b992e6a23511e5219862eb006
sidharth <sidhu005india@gmail.com>
2026-02-14 14:05:47 +0530

    commit by sid

--- abc.java
+++ abc.java
@@ -3,7 +3,10 @@ import java.util.stream.Collectors;
 
 public class abc {
 
-
+    public static void main(String[] args) {
+        System.out.println("inside intellij");
+        System.out.println("time kharab hai" );
+    }
 
 
 
